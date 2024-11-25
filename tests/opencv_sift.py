498acd9c8f3e75c5d71d3617ce73b3b953b25425
cny123222 <150103434+cny123222@users.noreply.github.com>
2024-11-25 15:16:16 +0800

    Update OpenCV-SIFT

--- tests/opencv_sift.py
+++ tests/opencv_sift.py
@@ -1,11 +1,24 @@
 import cv2
+import numpy as np
 
-gray = cv2.imread("target.jpg", cv2.IMREAD_GRAYSCALE)
+image1 = cv2.imread("target.jpg")
+image2 = cv2.imread("dataset/3.jpg")
+
+gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
 sift = cv2.SIFT_create()
-kp = sift.detect(gray, None)
-ret = cv2.drawKeypoints(gray, kp, None)
-kp, dst = sift.compute(gray, kp)
 
-cv2.imshow("SIFT Keypoints", ret)
+keypoints1, descriptors1 = sift.detectAndCompute(gray1, None)
+keypoints2, descriptors2 = sift.detectAndCompute(gray2, None)
+
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+
+good_matches = [m for m, n in matches if m.distance < 0.4 * n.distance]
+
+match_img = cv2.drawMatches(image1, keypoints1, image2, keypoints2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+cv2.imshow("SIFT Matches with Filtering", match_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
